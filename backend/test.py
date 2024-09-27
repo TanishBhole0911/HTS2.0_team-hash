a22ea6ac19f077da774ea0726df8d89195ca7bd8
Harry0786 <jayeshbansal100@Ggmail.com>
2024-09-27 16:14:56 +0530

    simple update

--- backend/test.py
+++ backend/test.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.cors import CORSMiddleware
 from routes.login import router as login_router
+from routes.register import router as register_router
 
 app = FastAPI()
 
