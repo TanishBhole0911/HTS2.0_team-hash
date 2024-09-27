21459c670a49f0f2becad6f87a0a9fd02342bfc3
Srijan Sahay Srivastava <70461251+SrijanSahaySrivastava@users.noreply.github.com>
2024-09-27 15:19:59 +0530

    login and mongoDB

--- backend/test.py
+++ backend/test.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.cors import CORSMiddleware
+from routes.login import router as login_router
 
 app = FastAPI()
 
