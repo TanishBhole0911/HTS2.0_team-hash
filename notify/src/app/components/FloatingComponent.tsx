528d215000443baaaee885e2b45ab45f1e950724
Adi <adityavyas670@gmail.com>
2024-09-28 11:58:53 +0530

    twitter Image

--- notify/src/app/components/FloatingComponent.tsx
+++ notify/src/app/components/FloatingComponent.tsx
@@ -2,89 +2,89 @@
 import { useSelector } from "react-redux";
 import { FloatingDoc } from "./FloatingDoc";
 import {
-    IconBrandGithub,
-    IconBrandX,
-    IconHome,
-    IconNewSection,
-    IconUser,
+  IconBrandGithub,
+  IconBrandX,
+  IconHome,
+  IconNewSection,
+  IconUser,
 } from "@tabler/icons-react";
 import "../globals.css";
 
 export function Floating() {
-    const user = useSelector((state: any) => state.user.user);
+  const user = useSelector((state: any) => state.user.user);
 
-    const links = user?.username
-        ? [
-            {
-                title: "Home",
-                icon: (
-                    <IconHome className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-                ),
-                href: "/",
-            },
-            {
-                title: "Projects",
-                icon: (
-                    <IconNewSection className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-                ),
-                href: "/Notes",
-            },
-            {
-                title: "Logout",
-                icon: (
-                    <IconUser className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-                ),
-                href: "/logout",
-            },
-            {
-                title: "Twitter",
-                icon: (
-                    <IconBrandX className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-                ),
-                href: "https://twitter.com/HackTheSpace_",
-            },
-            {
-                title: "GitHub",
-                icon: (
-                    <IconBrandGithub className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-                ),
-                href: "https://github.com/TanishBhole0911/HTS2.0_team-hash",
-            },
-        ]
-        : [
-            {
-                title: "Home",
-                icon: (
-                    <IconHome className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-                ),
-                href: "/",
-            },
-            {
-                title: "Register",
-                icon: (
-                    <IconUser className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-                ),
-                href: "/register",
-            },
-            {
-                title: "Login",
-                icon: (
-                    <IconUser className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-                ),
-                href: "/login",
-            },
-            {
-                title: "GitHub",
-                icon: (
-                    <IconBrandGithub className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-                ),
-                href: "https://github.com/TanishBhole0911/HTS2.0_team-hash",
-            },
-        ];
+  const links = user?.username
+    ? [
+        {
+          title: "Home",
+          icon: (
+            <IconHome className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+          ),
+          href: "/",
+        },
+        {
+          title: "Projects",
+          icon: (
+            <IconNewSection className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+          ),
+          href: "/Notes",
+        },
+        {
+          title: "Logout",
+          icon: (
+            <IconUser className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+          ),
+          href: "/logout",
+        },
+        {
+          title: "Twitter",
+          icon: (
+            <IconBrandX className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+          ),
+          href: "https://x.com/jayeshbansal07/status/1839910648592916752?s=46",
+        },
+        {
+          title: "GitHub",
+          icon: (
+            <IconBrandGithub className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+          ),
+          href: "https://github.com/TanishBhole0911/HTS2.0_team-hash",
+        },
+      ]
+    : [
+        {
+          title: "Home",
+          icon: (
+            <IconHome className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+          ),
+          href: "/",
+        },
+        {
+          title: "Register",
+          icon: (
+            <IconUser className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+          ),
+          href: "/register",
+        },
+        {
+          title: "Login",
+          icon: (
+            <IconUser className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+          ),
+          href: "/login",
+        },
+        {
+          title: "GitHub",
+          icon: (
+            <IconBrandGithub className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+          ),
+          href: "https://github.com/TanishBhole0911/HTS2.0_team-hash",
+        },
+      ];
 
-    return (
-        <div className="FloatingComponent">
-            <FloatingDoc items={links} mobileClassName="translate-y-20" />
-        </div>
-    );
+  return (
+    <div className="FloatingComponent">
+      <FloatingDoc items={links} mobileClassName="translate-y-20" />
+    </div>
+  );
 }
